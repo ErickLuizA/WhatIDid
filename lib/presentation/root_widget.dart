1f13943129b76584be0ceffce16a7e30e14e2ea4
erickLuizA <erickluiz69@gmail.com>
2021-04-26 14:59:22 -0300

    add: localization in root widget

--- lib/presentation/root_widget.dart
+++ lib/presentation/root_widget.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:whatidid/application/constants/app_routes.dart';
 import 'package:whatidid/application/constants/app_themes.dart';
+import 'package:whatidid/application/providers/controllers/home_category_controller.dart';
+import 'package:whatidid/application/providers/controllers/language_controller.dart';
+import 'package:whatidid/application/providers/controllers/theme_controller.dart';
 
-class RootWidget extends StatelessWidget {
+import 'app_localizations.dart';
+
+class RootWidget extends StatefulWidget {
+  @override
+  _RootWidgetState createState() => _RootWidgetState();
+}
+
+class _RootWidgetState extends State<RootWidget> {
+  @override
+  void initState() {
+    super.initState();
+
+    context.read(homeCategoryControllerProvider).load();
+    context.read(themeControllerProvider).load();
+    context.read(languageControllerProvider).loadLocale();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "WhatIDid",
-      theme: AppThemes.dark,
-      debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.SPLASH,
-      onGenerateRoute: onGenerateRoute,
+    return Consumer(
+      builder: (context, watch, child) {
+        final isDark = watch(themeControllerProvider).isDark;
+        final currentLocale = watch(languageControllerProvider).currentLocale;
+
+        return MaterialApp(
+          title: "WhatIDid",
+          theme: isDark ? AppThemes.dark : AppThemes.light,
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: [
+            LocalizationDelegate(),
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: [
+            const Locale('en', 'US'),
+            const Locale('pt', 'BR'),
+          ],
+          locale: currentLocale,
+          initialRoute: AppRoutes.SPLASH,
+          onGenerateRoute: onGenerateRoute,
+        );
+      },
     );
   }
 }
