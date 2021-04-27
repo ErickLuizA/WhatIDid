f6dd01877d7eff7197aa6166f5b3f796f832a681
erickLuizA <erickluiz69@gmail.com>
2021-04-26 22:56:07 -0300

    add: check if daily is in database to insert

--- lib/presentation/root_widget.dart
+++ lib/presentation/root_widget.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:whatidid/application/constants/app_routes.dart';
 import 'package:whatidid/application/constants/app_themes.dart';
 import 'package:whatidid/application/providers/controllers/home_category_controller.dart';
 import 'package:whatidid/application/providers/controllers/language_controller.dart';
 import 'package:whatidid/application/providers/controllers/theme_controller.dart';
+import 'package:whatidid/data/daos/categories_dao.dart';
+import 'package:whatidid/data/gateways/sembast_database.dart';
+import 'package:whatidid/domain/models/category.dart';
 
 import 'app_localizations.dart';
 
@@ -23,6 +27,23 @@ class _RootWidgetState extends State<RootWidget> {
     context.read(homeCategoryControllerProvider).load();
     context.read(themeControllerProvider).load();
     context.read(languageControllerProvider).loadLocale();
+    context.read(databaseProvider).openDatabase();
+
+    // Check if daily category is already in database
+    SharedPreferences.getInstance().then((value) {
+      final result = value.getBool('DAILY');
+
+      if (result == null) {
+        context.read(categoriesDaoProvider).insert(
+              Category(
+                image: 'assets/images/notebook.svg',
+                name: 'Daily',
+              ).toMap(),
+            );
+
+        value.setBool('DAILY', true);
+      }
+    });
   }
 
   @override
