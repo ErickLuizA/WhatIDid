a42d79c47fe983ab27af1554c680c482d1e074dd
erickLuizA <erickluiz69@gmail.com>
2021-04-26 22:55:17 -0300

    add: categories screen layout and category widget

--- lib/presentation/screens/categories/categories_screen.dart
+++ lib/presentation/screens/categories/categories_screen.dart
@@ -1,10 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:whatidid/application/providers/viewmodels/categories_viewmodel.dart'
+    hide State;
 
-class CategoriesScreen extends StatelessWidget {
+import 'widgets/category_widget.dart';
+
+class CategoriesScreen extends StatefulWidget {
+  @override
+  _CategoriesScreenState createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    context.read(categoriesViewModelProvider.notifier).getAll();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hello Categories"),
+    return ProviderListener(
+      provider: categoriesViewModelProvider,
+      onChange: (context, state) {
+        if (state is ErrorState) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Error loading categories"),
+            ),
+          );
+        }
+      },
+      child: Consumer(
+        builder: (context, watch, child) {
+          final state = watch(categoriesViewModelProvider);
+
+          if (state is LoadingState) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (state is EmptyState) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "There is nothing here",
+                  style: Theme.of(context).primaryTextTheme.bodyText1,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Press the",
+                      style: Theme.of(context).primaryTextTheme.bodyText1,
+                    ),
+                    Icon(
+                      Icons.add_circle,
+                      color: Theme.of(context).accentColor,
+                    ),
+                    Text(
+                      "to add a entrie",
+                      style: Theme.of(context).primaryTextTheme.bodyText1,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                SvgPicture.asset(
+                  'assets/images/empty.svg',
+                  width: MediaQuery.of(context).size.width / 2,
+                ),
+              ],
+            );
+          }
+
+          if (state is LoadedState) {
+            return GridView.count(
+              crossAxisCount: 2,
+              semanticChildCount: state.result.length,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              children: state.result.map((e) => CategoryWidget(e)).toList(),
+            );
+          }
+
+          return Container();
+        },
+      ),
     );
   }
 }
---
+++ lib/presentation/screens/categories/widgets/category_widget.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:whatidid/domain/models/category.dart';
+import 'package:whatidid/presentation/app_localizations.dart';
+
+class CategoryWidget extends StatelessWidget {
+  final Category category;
+
+  const CategoryWidget(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    print(category);
+
+    return Column(
+      children: [
+        showImage(),
+        showText(context),
+      ],
+    );
+  }
+
+  Widget showText(BuildContext context) {
+    if (category.name == 'Daily') {
+      return Text(AppLocalizations.of(context).translate('daily'));
+    }
+
+    return Text(category.name);
+  }
+
+  Widget showImage() {
+    if (category.image.contains('assets/')) {
+      return SvgPicture.asset(category.image);
+    }
+
+    return Image.file(
+      File(category.image),
+    );
+  }
+}
