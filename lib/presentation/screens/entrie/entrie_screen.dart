444905f6e68c56c24168f3891d7bc338005484b9
erickLuizA <erickluiz69@gmail.com>
2021-05-03 14:23:39 -0300

    add entrie screen

--- lib/presentation/screens/entrie/entrie_screen.dart
+++ lib/presentation/screens/entrie/entrie_screen.dart
@@ -1,5 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:whatidid/application/providers/viewmodels/entries_viewmodel.dart';
 import 'package:whatidid/domain/models/entrie.dart';
+import 'package:whatidid/presentation/utils/num_to_month.dart';
+import 'package:whatidid/presentation/utils/num_to_weekday.dart';
 
 class EntrieScreen extends StatelessWidget {
   final Entrie entrie;
@@ -8,8 +15,139 @@ class EntrieScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width / 3;
+    final height = MediaQuery.of(context).size.height / 4;
+
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(height),
+        child: Stack(
+          children: [
+            AppBar(
+              actions: [
+                PopupMenuButton(
+                  onSelected: (String selected) async {
+                    if (selected == "delete") {
+                      final res = await context
+                          .read(entriesViewModelProvider.notifier)
+                          .delete(entrie.id!);
+
+                      if (res) {
+                        if (Navigator.canPop(context)) {
+                          context
+                              .read(entriesViewModelProvider.notifier)
+                              .getAll(entrie.category);
+
+                          Navigator.pop(context);
+                        }
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content:
+                                Text("Error while trying to delete entrie"),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                  itemBuilder: (context) {
+                    return [
+                      PopupMenuItem(
+                        value: "delete",
+                        child: Text("Delete"),
+                      ),
+                    ];
+                  },
+                ),
+              ],
+            ),
+            Center(
+              child: showImage(context, width, height),
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text.rich(
+                TextSpan(
+                  text: "${numToWeekday(entrie.dateTime.day)}, ",
+                  children: [
+                    TextSpan(
+                      text: "${entrie.dateTime.day} ",
+                    ),
+                    TextSpan(
+                      text: "${numToMonth(entrie.dateTime.month)} ",
+                    ),
+                    TextSpan(
+                      text: "${entrie.dateTime.year} ",
+                    ),
+                  ],
+                ),
+                style: Theme.of(context).primaryTextTheme.headline5,
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Icon(
+                    Icons.access_time,
+                    color: Theme.of(context).primaryTextTheme.bodyText2?.color,
+                  ),
+                  Text(
+                    "${entrie.dateTime.hour}:${entrie.dateTime.minute}",
+                    style: Theme.of(context).primaryTextTheme.bodyText2,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    "${entrie.category}",
+                    style: Theme.of(context).primaryTextTheme.bodyText2,
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                entrie.title,
+                style: TextStyle(
+                  color: Theme.of(context).accentColor,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "${entrie.content}",
+                style: Theme.of(context).primaryTextTheme.bodyText1,
+                softWrap: true,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  Widget showImage(BuildContext context, double width, double height) {
+    if (entrie.image == null) {
+      return SvgPicture.asset(
+        'assets/images/notebook.svg',
+      );
+    } else {
+      return Image.file(
+        File(entrie.image!),
+        errorBuilder: (
+          BuildContext context,
+          Object exception,
+          StackTrace? stackTrace,
+        ) {
+          return SvgPicture.asset(
+            'assets/images/notebook.svg',
+          );
+        },
+      );
+    }
   }
 }
